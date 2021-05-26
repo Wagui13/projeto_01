3e692130a9fe80e9b1965d0b0c93f2bf7401b0ef
icarogoggin <icaro.goggin@gmail.com>
2021-05-25 22:39:34 -0300

    Create projeto_prova.py

---
+++ projeto_prova.py
@@ -0,0 +1,30 @@
+class relogio:
+    def __init__(self):
+        self.horas = 23
+        self.minutos = 59  
+    def __str__(self):
+        return f"faltam{self.horas:02d}:{self.minutos:02d} para entregar o projeto"
+    def custo_tempo(self, minutos):
+
+    def atrasado(self):
+
+class programador:
+    def __init__(self):
+
+    def __str__(self):
+
+    def dormir(self):
+
+
+class trabalho:
+    def __init__(self):
+
+    def eventos_trabalho(self):
+
+
+class casa:
+    def __init__(self):
+
+    def eventos_casa(self):
+        
+class projeto:
