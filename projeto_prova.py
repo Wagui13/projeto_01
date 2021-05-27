894e76124f88bc039a976551d76b209ec72983c9
icarogoggin <icaro.goggin@gmail.com>
2021-05-26 22:32:03 -0300

    Update projeto_prova.py

--- projeto_prova.py
+++ projeto_prova.py
@@ -2,18 +2,36 @@ class relogio:
     def __init__(self):
         self.horas = 23
         self.minutos = 59  
-    def __str__(self):
+    def contagem_regressiva(self):
         return f"faltam{self.horas:02d}:{self.minutos:02d} para entregar o projeto"
-    def custo_tempo(self, minutos):
-
-    def atrasado(self):
+    def custo_tempo(self, minutos): #operações maiores que 60 minutos dá erro
+        self.minutos -= minutos
+        if self.minutos < 0:
+            self.horas += self.minutos // 60
+            self.minutos = self.minutos + 60
+# a = relogio()
+# while True:
+#     tempo = int(input('Quantos minutos ? '))
+#     a.custo_tempo(tempo)
+#     print (a.minutos)
+#     print (a.horas)
+#     def atrasado(self):
 
 class programador:
-    def __init__(self):
+    def __init__(self, energia, sono):
+        self.energia = 10
+        self.sono = 0
+    
+    def energia(self,s):
+        if self.energia > 0:
+            s = int(input('Digite a energia'))
+            self.energia = self.energia + s
 
-    def __str__(self):
+        
 
-    def dormir(self):
+    #def __str__(self):
+
+    #def dormir(self):
 
 
 class trabalho:
